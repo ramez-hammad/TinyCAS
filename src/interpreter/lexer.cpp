b043ac4baaa0dab5983094217f021c6a321ab9bd
ramez-hammad <hammad.ramez001@gmail.com>
2025-09-14 21:33:27 +0300

    Tokenize variables, functions, other symbols

--- src/interpreter/lexer.cpp
+++ src/interpreter/lexer.cpp
@@ -3,14 +3,37 @@
 
 #include "lexer.h"
 
-Token create_token_num(double val)
-{
-    return {TokenType::TokenNum, val};
-}
+Token create_token_num(double val) { return {TokenType::TokenNum, val}; }
 
-Token create_token_op(TokenType type)
+Token create_token_op(TokenType type) { return {type}; }
+
+Token create_token_func(TokenType type, std::string arg) { return {type, 0, std::move(arg)}; }
+
+Token create_token_var(char sym) { return {TokenType::TokenVar, 0, "", sym}; }
+
+// Get function argument
+std::string get_func_arg(std::string expr, size_t pos)
 {
-    return {type};
+    std::string arg;
+    int num_paren = 0;
+
+    for (; pos < expr.length(); pos++) {
+        if (expr.at(pos) == '(') {
+            num_paren++;
+            arg.push_back(expr[pos]);
+        } else if (expr.at(pos) == ')') {
+            num_paren--;
+            if (num_paren >= 0) {
+                arg.push_back(expr[pos]);
+            } else {
+                break;
+            }
+        } else {
+            arg.push_back(expr[pos]);
+        }
+    }
+
+    return arg;
 }
 
 std::vector<Token> tokenize(std::string expr)
@@ -44,7 +67,7 @@ std::vector<Token> tokenize(std::string expr)
         }
 
         switch (expr.at(pos)) {
-            // Operators
+            // Symbols
             case '+':
                 token_vector.push_back(create_token_op(TokenType::TokenPlus));
                 break;
@@ -60,6 +83,148 @@ std::vector<Token> tokenize(std::string expr)
             case '^':
                 token_vector.push_back(create_token_op(TokenType::TokenPow));
                 break;
+            case '!':
+                token_vector.push_back(create_token_op(TokenType::TokenFactorial));
+                break;
+            case '(':
+                token_vector.push_back(create_token_op(TokenType::TokenLParen));
+                break;
+            case ')':
+                token_vector.push_back(create_token_op(TokenType::TokenRParen));
+                break;
+            case '=':
+                token_vector.push_back(create_token_op(TokenType::TokenEqual));
+                break;
+
+            // Functions
+            case 's':
+                if (expr.at(pos + 3) == '(') {
+                    // sin
+                    token_vector.push_back(create_token_func(TokenType::TokenSin, get_func_arg(expr, pos + 4)));
+
+                    pos += 4;
+                    pos += token_vector.back().arg.length();
+                } else if (expr.at(pos + 4) == '(') {
+                    // sinh
+                    token_vector.push_back(create_token_func(TokenType::TokenSinh, get_func_arg(expr, pos + 5)));
+
+                    pos += 5;
+                    pos += token_vector.back().arg.length();
+                } else {
+                    // sqrt
+                    token_vector.push_back(create_token_func(TokenType::TokenSqrt, get_func_arg(expr, pos + 5)));
+
+                    pos += 5;
+                    pos += token_vector.back().arg.length();
+                }
+
+                break;
+            case 'c':
+                if (expr.at(pos + 4) == '(') {
+                    // cos
+                    token_vector.push_back(create_token_func(TokenType::TokenCos, get_func_arg(expr, pos + 5)));
+
+                    pos += 5;
+                    pos += token_vector.back().arg.length();
+                } else {
+                    // cosh
+                    token_vector.push_back(create_token_func(TokenType::TokenCosh, get_func_arg(expr, pos + 5)));
+
+                    pos += 5;
+                    pos += token_vector.back().arg.length();
+                }
+
+                break;
+            case 't':
+                if (expr.at(pos + 3) == '(') {
+                    // tan
+                    token_vector.push_back(create_token_func(TokenType::TokenTan, get_func_arg(expr, pos + 4)));
+
+                    pos += 4;
+                    pos += token_vector.back().arg.length();
+                } else {
+                    // tanh
+                    token_vector.push_back(create_token_func(TokenType::TokenCos, get_func_arg(expr, pos + 5)));
+
+                    pos += 5;
+                    pos += token_vector.back().arg.length();
+                }
+
+                break;
+            case 'a':
+                if (expr.at(pos + 2) == 's') {
+                    // abs
+                    token_vector.push_back(create_token_func(TokenType::TokenAbs, get_func_arg(expr, pos + 4)));
+
+                    pos += 4;
+                    pos += token_vector.back().arg.length();
+                } else if (expr.at(pos + 1) == 's') {
+                    if (expr.at(pos + 4) == '(') {
+                        // asin
+                        token_vector.push_back(create_token_func(TokenType::TokenAsin, get_func_arg(expr, pos + 5)));
+
+                        pos += 5;
+                        pos += token_vector.back().arg.length();
+                    } else {
+                        // asinh
+                        token_vector.push_back(create_token_func(TokenType::TokenAsinh, get_func_arg(expr, pos + 6)));
+
+                        pos += 6;
+                        pos += token_vector.back().arg.length();
+                    }
+                } else if (expr.at(pos + 1) == 'c') {
+                    if (expr.at(pos + 4) == '(') {
+                        // acos
+                        token_vector.push_back(create_token_func(TokenType::TokenAcos, get_func_arg(expr, pos + 5)));
+
+                        pos += 5;
+                        pos += token_vector.back().arg.length();
+                    } else {
+                        // acosh
+                        token_vector.push_back(create_token_func(TokenType::TokenAcosh, get_func_arg(expr, pos + 6)));
+
+                        pos += 6;
+                        pos += token_vector.back().arg.length();
+                    }
+                } else {
+                    // t
+                    if (expr.at(pos + 4) == '(') {
+                        // atan
+                        token_vector.push_back(create_token_func(TokenType::TokenAtan, get_func_arg(expr, pos + 5)));
+
+                        pos += 5;
+                        pos += token_vector.back().arg.length();
+                    } else {
+                        // atanh
+                        token_vector.push_back(create_token_func(TokenType::TokenAtanh, get_func_arg(expr, pos + 6)));
+
+                        pos += 6;
+                        pos += token_vector.back().arg.length();
+                    }
+                }
+
+                break;
+            case 'l':
+                if (expr.at(pos + 1) == 'n') {
+                    // ln
+                    token_vector.push_back(create_token_func(TokenType::TokenLog, get_func_arg(expr, pos + 3)));
+
+                    pos += 3;
+                    pos += token_vector.back().arg.length();
+                } else {
+                    // log
+                    token_vector.push_back(create_token_func(TokenType::TokenLog, get_func_arg(expr, pos + 4)));
+
+                    pos += 4;
+                    pos += token_vector.back().arg.length();
+                }
+                break;
+        }
+
+        // Variables
+        // A-Z, x, pi, e
+        if ((expr.at(pos) >= 65 && expr.at(pos) <= 90) || expr.at(pos) == 'x' || expr.at(pos) == 'p' || expr.at(pos) == 'e') {
+            token_vector.push_back(create_token_var(expr.at(pos)));
         }
 
         pos++;
--- src/interpreter/lexer.h
+++ src/interpreter/lexer.h
@@ -5,9 +5,35 @@ enum class TokenType
     TokenNum,
     TokenPlus,
     TokenMinus,
+
+    // Unary operators are only identified in the parser
+    TokenUnaryMinus,
+    TokenUnaryPlus,
+
     TokenMul,
     TokenDiv,
     TokenPow,
+    TokenFactorial,
+    TokenLParen,
+    TokenRParen,
+    TokenEqual,
+    TokenSin,
+    TokenCos,
+    TokenTan,
+    TokenSinh,
+    TokenCosh,
+    TokenTanh,
+    TokenAsin,
+    TokenAcos,
+    TokenAtan,
+    TokenAsinh,
+    TokenAcosh,
+    TokenAtanh,
+    TokenLn,
+    TokenLog,
+    TokenSqrt,
+    TokenAbs,
+    TokenVar,
     TokenNull
 };
 
@@ -15,7 +41,9 @@ class Token
 {
 public:
     TokenType type;
-    double val;
+    double val; // For numbers
+    std::string arg; // For functions
+    char sym; // For variables
 };
 
 
